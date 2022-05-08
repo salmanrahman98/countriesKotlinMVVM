1003a1659240314fea772703af9930e96ba84c68
Salman <salmanrahman1998@gmail.com>
2022-05-08 13:50:41 +0530

    first kommit

---
+++ app/src/main/java/com/rahman/countrymvvm/View/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.rahman.countrymvvm.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rahman.countrymvvm.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/rahman/countrymvvm/viewmodel/listViewModel.kt
@@ -0,0 +1,32 @@
+package com.rahman.countrymvvm.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class listViewModel : ViewModel() {
+
+    val countriesMutable = MutableLiveData<List<Country>>()
+    val countryLoadError = MutableLiveData<Boolean>()
+    val loadingProgressBar = MutableLiveData<Boolean>()
+
+    fun refresh() {
+        fetchCountries()
+    }
+
+    private fun fetchCountries() {
+        val mockData = listOf(
+            Country("Country 1"),
+            Country("Country 2"),
+            Country("Country 3"),
+            Country("Country 4"),
+            Country("Country 5"),
+            Country("Country 6"),
+            Country("Country 7"),
+            Country("Country 8"),
+        )
+
+        countryLoadError.value = false
+        loadingProgressBar.value = false
+        countriesMutable.value = mockData
+    }
+}
